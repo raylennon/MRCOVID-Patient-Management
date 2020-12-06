776545272aadf4aa003fe2f508cee9e3e1abce38
raylennon <48305386+raylennon@users.noreply.github.com>
2020-12-05 21:58:34 -0500

    Added Stats page to app

--- src/home.java
+++ src/home.java
@@ -3,6 +3,15 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.scene.image.Image;
@@ -12,7 +21,39 @@ public class home extends Application {
 
     private static final Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
+    private List<String> getRecordFromLine(String line) {
+        List<String> values = new ArrayList<String>();
+        try (Scanner rowScanner = new Scanner(line)) {
+            rowScanner.useDelimiter(", ");
+            while (rowScanner.hasNext()) {
+                values.add(rowScanner.next());
+            }
+        }
+        return values;
+    }
+
     public static void main(String[] args) {
+
+        File file = new File(".");
+        for(String fileNames : file.list()) System.out.println(fileNames);
+
+        List<List<String>> records = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader("./data/patients.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(", ");
+                records.add(Arrays.asList(values));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for(List<String> list : records) {
+            for (String val : list) {
+                System.out.print(val + " ");
+            } System.out.println();
+        }
+
         launch(args);
     }
 
